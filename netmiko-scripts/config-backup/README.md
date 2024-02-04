36a98af6754d14271fa8533f71f9e8997747fcfb
Matt <matthewabel223@gmail.com>
2024-02-04 10:40:38 -0500

    Netmike backup-config script

---
+++ netmiko-scripts/config-backup/README.md
@@ -0,0 +1,18 @@
+## Telnet Config Backup
+### Prerequisites
+python
+netmiko
+SSH enabled on the network device
+### Description
+Record the "show running-config" of the devices listed in the Netmiko python script. Log the output to a file. To run this script, configure your network devices in the python script.  Filling in device_type, ip, username, and password:
+
+'''example_device_X = {
+    'device_type': 'cisco_ios',
+    'ip': 'ip_addr',
+    'username': 'username',
+    'password': 'password'
+}'''
+
+Add the devices to the list of devices:
+'''devices = [example_device_1, example_device_2, example_device_etc]'''
+ 
---
+++ netmiko-scripts/netmiko-bulk-config.py
@@ -0,0 +1,29 @@
+from logging import config
+from netmiko import ConnectHandler
+
+example_device_1 = {
+    'device_type': 'cisco_ios',
+    'ip': 'ip_addr',
+    'username': 'username',
+    'password': 'password'
+}
+
+example_device_2 = {
+    'device_type': 'cisco_ios',
+    'ip': 'ip_addr',
+    'username': 'username',
+    'password': 'password'
+}
+
+# Configure devices above, add newly configured devices to list of devices
+devices = [example_device_1]
+
+for device in devices:
+    net_connect = ConnectHandler(**device)
+    config_commands = ["do show run\n"]
+    output = net_connect.send_config_set(config_commands)
+    f = open(f"config_{device['ip']}", "w")
+    f.write(output)
+
+
+print(output)
